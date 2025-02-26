8d104ece0d86d25a33b98cf825dd1cdd218c1eb0
Adwok Adiebo <adwok.adiebo@stu.ocsb.ca>
2025-02-26 01:41:55 +0000

    areaPerRectInput.cpp

---
+++ areaPerRectInput.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2025 Adwok Adiebo all rights reserved.
+// .
+// Created by: Adwok Adiebo
+// Created on: Feb 25, 2025
+// This program calculates and displays .
+// the area and perimeter of the rectangle.
+// The rectangle has a length of 12cm and width of 6cm
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+// The code below illustrates the length and width of the rectangle
+    std::cout << "A rectangle has length of 12cm and width of 6cm" << std::endl;
+// The code below demonstrates the steps to find and solve
+// the area of the rectangle with the given dimensions
+    std::cout << "Area :" << (12 * 6) << "cm^2" << std::endl;
+// The code below demonstrates the steps to find and solve
+// the perimeter of the rectangle with the given dimensions
+    std::cout << "Perimeter :" << ((12 + 12) + (6 + 6)) << "cm" << std::endl;
+}
